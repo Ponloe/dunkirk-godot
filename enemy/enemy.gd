027f3c157fa616f85e6d9d83b1edb4571c0f74c8
Soponloe <sovannsoponloe@gmail.com>
2026-03-02 14:12:36 +0700

    tweaked explosion

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,13 +5,49 @@ signal killed(points)
 signal hit
 
 @export var speed: float = 150
-@export var hp: int = 1 
+@export var hp: int = 1
 @export var points: int = 50
 
-func _physics_process(delta: float) -> void:
-	global_position.y += speed * delta 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var explosion: AnimatedSprite2D = $Explosion
 
-func die(): 
+var is_dying := false
+
+func _ready() -> void:
+	explosion.visible = false
+
+func _physics_process(delta: float) -> void:
+	if is_dying:
+		return
+	global_position.y += speed * delta
+
+func take_damage(amount: int) -> void:
+	if is_dying:
+		return
+
+	hp -= amount
+	if hp <= 0:
+		killed.emit(points)
+		die()
+	else:
+		hit.emit()
+
+func die() -> void:
+	is_dying = true
+
+	# Disable enemy visuals and collision
+	sprite.visible = false
+	collision.disabled = true
+
+	# Play explosion
+	explosion.visible = true
+	explosion.play("explode")
+
+	# Remove enemy after explosion finishes
+	explosion.animation_finished.connect(_on_explosion_finished)
+
+func _on_explosion_finished() -> void:
 	queue_free()
 
 func _on_body_entered(body):
@@ -20,12 +56,5 @@ func _on_body_entered(body):
 		die()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free() 
-
-func take_damage(amount: int) -> void: 
-	hp -= amount 
-	if hp <= 0:
-		killed.emit(points)
-		die()
-	else: 
-		hit.emit()
+	if not is_dying:
+		queue_free()
